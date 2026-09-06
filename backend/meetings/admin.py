05578984825cc3bcdc45cc4147a4c3f4ebaa13c2
Huzaifa Ahmed <ahmedhuzaifa368@gmail.com>
2026-09-06 14:44:31 -0700

    Complete django part

--- backend/meetings/admin.py
+++ backend/meetings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Meeting, TranscriptSegment, ChatMessage
 
-# Register your models here.
+admin.site.register(Meeting)
+admin.site.register(TranscriptSegment)
+admin.site.register(ChatMessage)
